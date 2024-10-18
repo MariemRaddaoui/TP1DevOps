301aedb02e662f1936b2310db08c424c275c0f77
MariemRaddaouiTendanz <mariem.internship@tendanz.com>
2024-10-18 15:13:27 +0100

    modification de branch

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World! - Test"
+    return "Hello, World! _ Test"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
